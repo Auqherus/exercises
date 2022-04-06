661bc2224a1266ebaf29223b507f2f43e9ec67a4
Auqherus <patrykromanowski@gmail.com>
2022-04-06 21:52:12 +0200

    2. Reversed Strings

--- src/Simple_project1.java
+++ src/Simple_project1.java
@@ -4,25 +4,20 @@ import java.io.*;
 class Simple_project1 {
 
 
-    public static int FirstFactorial(int num) {
-        // code goes here
+        public static String FirstReverse (String str) {
+            // code goes here
+            String reverse = "";
+            for (int i =str.length()-1; i>=0 ; i--) {
+                reverse = reverse+str.charAt(i);
+            }
 
-
-        int result = 1;
-        for (int i = 1; i <= num; i++) {
-
-            result =result * i;
-
+            return reverse;
         }
 
-        return result;
+        public static void main (String[]args){
+            // keep this function call here
+            Scanner s = new Scanner(System.in);
+            System.out.print("Put a word to reverse :" +" ");
+            System.out.print("Result :"+" "+FirstReverse(s.nextLine()));
+        }
     }
-
-    public static void main (String[] args) {
-        // keep this function call here
-        Scanner s = new Scanner(System.in);
-        System.out.print("Put a number here : ");
-        System.out.print("Total : "+ " "+FirstFactorial(Integer.parseInt(s.nextLine())));
-    }
-
-}
