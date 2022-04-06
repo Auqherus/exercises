107fc584c95ed5d2fc8834a3eb2e6b746203fdc8
Auqherus <patrykromanowski@gmail.com>
2022-04-06 13:38:35 +0200

    2. String of numbers multiplies

--- src/Simple_project1.java
+++ src/Simple_project1.java
@@ -1,41 +1,28 @@
-import java.math.BigInteger;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Scanner;
-import java.util.concurrent.TimeUnit;
+import java.util.*;
+import java.io.*;
 
-public class Simple_project1 {
+class Simple_project1 {
 
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter start time (HH:mm aa): ");
-        String starttime = input.nextLine();
+    public static int FirstFactorial(int num) {
+        // code goes here
 
-        System.out.print("Enter end time (HH:mm aa): ");
-        String endtime = input.nextLine();
 
-//HH converts hour in 24 hours format (0-23), day calculation
-        SimpleDateFormat format = new SimpleDateFormat("HH:mm aa");
+        int result = 1;
+        for (int i = 1; i <= num; i++) {
 
-        Date d1 = null;
-        Date d2 = null;
+            result =result * i;
 
-        try {
-            d1 = format.parse(starttime);
-            d2 = format.parse(endtime);
-
-            //in milliseconds
-            long diff = d2.getTime() - d1.getTime();
-
-            long diffSeconds = diff / 1000 % 60;
-            long diffMinutes = diff / (60 * 1000) % 60;
-
-            System.out.print(diffMinutes + " minutes and " + diffSeconds + " seconds.");
-        } catch (Exception e) {
-            System.out.println("Invalid fromat");
         }
-    }
-}
 
+        return result;
+    }
+
+    public static void main (String[] args) {
+        // keep this function call here
+        Scanner s = new Scanner(System.in);
+        System.out.print("Put a number here : ");
+        System.out.print("Total : "+ " "+FirstFactorial(Integer.parseInt(s.nextLine())));
+    }
+
+}
