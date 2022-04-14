38ff431391cfb6cc601fe1d086bc6847cc490307
Auqherus <patrykromanowski@gmail.com>
2022-04-14 15:27:38 +0200

    2. Count chars v2

--- src/Simple_project1.java
+++ src/Simple_project1.java
@@ -1,64 +1,43 @@
-import java.util.*;
+import java.util.Scanner;
 
-class Simple_project1 {
+public class Simple_project1 {
 
 
     public static String CharCounts(String start) {
 
+        int endOfLine = start.length();
 
-        String spaceString = "";
-        boolean answer = false;
-        int countWords = 0;
-        int countSpaces = 0;
-        int result = 0;
+        int counter = 0;
+        boolean ifTrue = true;
+        String result = "";
+        int k = 0;
 
-        for (int i = 0; i < start.length(); i++) {
+        for (int i = 0; i < endOfLine; i++) {
 
-            if(Character.isLetter(start.charAt(i)) && i != start.length()-1){
-                answer = true;
-            }
-           else if(!Character.isLetter(start.charAt(i)) && answer){
-               countWords++;
-               answer = false;
-           }
-           else if(Character.isLetter(start.charAt(i)) && i == start.length()-1){
-               countWords++;
-            }
-            if(start.charAt(i) == ' '){
-                countSpaces++;
+            char a = start.charAt(i);
+            k = 1;
+
+            for (int j = 0; j < i; j++) {
+
+                char b = start.charAt(j);
+                if (a == b && i!= start.length()) {
+                    k++;
+                }
+
             }
 
+            result = result + a + k;
         }
 
-        System.out.println("Words :" +countWords);
-        System.out.println("Spaces :" +countSpaces);
-
-      /*  System.out.println(result);*/
-
-        return spaceString;
-    }
+            return result;
+        }
 
 
-    public static void main(String[] args) {
+        public static void main (String[]args){
 
-        Scanner TryMe = new Scanner(System.in);
-
-        while (true) {
-            System.out.print("Put string of letters here to count(ex.'aaabfrtd') :" + " ");
-            String s = TryMe.nextLine();
-            if (s.equals("stop")) {
-                break;
-            }
-           CharCounts(s);
+            Scanner TryMe = new Scanner(System.in);
+            System.out.print("Wpisz przykladowe zdanie :" + " ");
+            System.out.println(CharCounts(TryMe.nextLine()));
 
         }
     }
-}
-
-
-
-
-
-
-
-
